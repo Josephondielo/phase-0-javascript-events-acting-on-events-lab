672adc4cd1c95d0f8100e73f95d65f850710e434
Josephondielo <josebonagain@gmail.com>
2025-07-08 16:45:12 +0300

    passed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+//moving to the left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+//moving to the right
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
